756cf421132ec45191f1e8ae6dbcb74944b4b8d3
Bailey Yi <bzy203@nyu.edu>
2025-12-18 19:11:31 -0800

    Added users routes and ignored Python bytecode

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+from .models import User
+
+def index(request):
+    latest_user_list = User.objects.all()[:3]
+    output = "".join(user.email for user in latest_user_list)
+    return HttpResponse(output)
