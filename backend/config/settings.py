08133b5a01e92dfa1b8f3056679e943207a09536
Bailey Yi <bzy203@nyu.edu>
2025-12-19 14:31:33 -0800

    Created user serializer

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "rest_framework",
     "api.apps.ApiConfig",
     "users.apps.UsersConfig",
     "accountants.apps.AccountantsConfig",
@@ -42,6 +43,15 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
 
-from .models import User
 
-def index(request):
-    latest_user_list = User.objects.all()[:3]
-    output = "".join(user.email for user in latest_user_list)
-    return HttpResponse(output)
+@api_view(["GET", "POST"])
+@permission_classes([AllowAny])
+def user_list(request):
+    if request.method == "GET":
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    elif request.method == "POST":
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
