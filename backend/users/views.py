a75e8d7d69f5ba78bb8fdcc26018a0976cda9a00
Bailey Yi <bzy203@nyu.edu>
2025-12-27 06:47:24 -0800

    Added basic signup endpoint (model, serializer, view, urls)

--- backend/users/views.py
+++ backend/users/views.py
@@ -2,23 +2,39 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
+
 
 from users.models import User
 from users.serializers import UserSerializer
 
 
-@api_view(["GET", "POST"])
-@permission_classes([AllowAny])
-def user_list(request):
-    if request.method == "GET":
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
+# @api_view(["GET", "POST"])
+# @permission_classes([AllowAny])
+# def user_list(request):
+#     if request.method == "GET":
+#         users = User.objects.all()
+#         serializer = UserSerializer(users, many=True)
+#         return Response(serializer.data)
 
-    elif request.method == "POST":
+#     elif request.method == "POST":
+#         print("in the post method of get user list")
+#         serializer = UserSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+#Allows user to signup
+class SignUp(APIView):
+    permission_classes=[AllowAny]
+
+    def post(self, request):
+        #instantiate serializer 
         serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
+        if serializer.is_valid(raise_exception=True):
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
