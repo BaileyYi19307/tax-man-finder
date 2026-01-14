cb7a81ec7a56680fae536ac4505f4275342195d9
Bailey Yi <bzy203@nyu.edu>
2026-01-14 18:21:48 -0500

    Added chat consumers, routing based on django channels tutorial; needs to be upgraded to private chatting

--- backend/chats/models.py
+++ backend/chats/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users import User
 
 # Create your models here.
+class Conversation(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name='from_user')
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE,
+                                  related_name='to_user')
+    text = models.TextField()
+    #file 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
