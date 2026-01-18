880cd901bbb77e0503f4face5efb2537eb4cf9ca
Bailey Yi <bzy203@nyu.edu>
2026-01-18 11:52:26 -0500

    Added custom UserAdmin model so that admin pages uses email instead of username

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,6 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models he
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 from .models import User
 
-admin.site.register(User)
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ("email",)
+    list_display = ("email", "is_staff", "is_active", "is_verified")
+    search_fields = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Status", {"fields": ("is_active", "is_verified", "is_staff", "is_superuser")}),
+        ("Roles", {"fields": ("is_accountant",)}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
