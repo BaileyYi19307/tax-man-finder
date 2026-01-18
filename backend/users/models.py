f9bc1ff6232f09e001240e71b6d3f23425fa93be
Bailey Yi <bzy203@nyu.edu>
2026-01-18 11:54:27 -0500

    Switched User Model to email based auth and added verification fields

--- backend/users/models.py
+++ backend/users/models.py
@@ -3,8 +3,26 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
+    #abstract user already has
+    #id, username, email, password, last_login
+    #is_active, is_staff, is_superuser, date_joined
+    #first_name, last_name
+    email = models.EmailField(unique=True,blank=True)
+    username = None # want email only identity
+    USERNAME_FIELD="email"
+    REQUIRED_FIELDS=[]
+
     is_accountant = models.BooleanField(default=False)
 
+    #have to implement is_active on the chosen auth backend
+    is_verified = models.BooleanField(default=False)
+    updated_at = models.DateTimeField(auto_now = True)
+    #updates the field with current date and time every time the model instance's save() method is called
+
+    phone_number = models.CharField(max_length=20,blank=True)# field is not required in forms 
+    # profile_photo = models.ImageField()
+
+
     def __str__(self):
-        return self.email or self.username
+        return self.email
 
