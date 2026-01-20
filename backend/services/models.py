7d11bb62842950204437bf85fc0e3f3b3689e433
Bailey Yi <bzy203@nyu.edu>
2026-01-19 20:21:47 -0500

    Updated Service model to refer to User model for accountant instead of AccountantProfile

--- backend/services/models.py
+++ backend/services/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.conf import settings
 # Create your models here.
 class Service(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=8,decimal_places=2)
-    accountant = models.ForeignKey("accountants.AccountantProfile", on_delete=models.CASCADE, related_name="services")
+    accountant = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="services")
 
     def __str__(self):
         return self.name
