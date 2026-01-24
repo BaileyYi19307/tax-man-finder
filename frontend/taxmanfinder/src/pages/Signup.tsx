f8111ee632d155df8a7c860f264f7a3eebc63771
Bailey Yi <bzy203@nyu.edu>
2026-01-24 17:43:06 -0500

    Added React Routing for multiple page routing; added Dashboard, Home, and Signup components

--- frontend/taxmanfinder/src/pages/Signup.tsx
+++ frontend/taxmanfinder/src/pages/Signup.tsx
@@ -1,19 +1,28 @@
+// fields=["email","password","role","id"]
+
 import { useState } from "react";
 import axios from "axios";
+import {Link} from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
-export default function LoginPage() {
+// <Link to="/"> Home >/Link>
+
+export default function SignUpPage() {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [role,setRole]=useState("");
 
   async function loginUser(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
     try {
       const response = await axios.post(
-        "http://127.0.0.1:8000/users/auth/login/",
+        "http://127.0.0.1:8000/users/auth/signup/",
         {
           email: email,
           password: password,
+          role:role
         },
         {
           headers: {
@@ -23,6 +32,9 @@ export default function LoginPage() {
       );
 
       console.log("success:", response.data);
+      //send them to login page
+      navigate("/login",{replace:true});
+
     } catch (err: any) {
         console.log("status:", err.response?.status);
         console.log("data:", err.response?.data);
@@ -49,8 +61,20 @@ export default function LoginPage() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
+            <label htmlFor="role">Role</label>
+            <select
+            id="role"
+            name="role"
+            value={role}
+            onChange={(e) => setRole(e.target.value)}
+            >
+            <option value="">Select a role</option>
+            <option value="accountant">Accountant</option>
+            <option value="client">Client</option>
+            </select>
 
-        <button type="submit">Login</button>
+
+        <button type="submit">Signup</button>
       </form>
     </div>
   );
