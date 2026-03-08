a7729551bd9117e4ba3cdd65c99f90e07f739da0
Bailey Yi <bzy203@nyu.edu>
2026-03-07 20:09:48 -0500

    Restore booking creation backend

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,11 +17,6 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView
-)
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
@@ -30,11 +25,5 @@ urlpatterns = [
     path("bookings/",include("bookings.urls")),
     path("accountants/",include("accountants.urls")),
     path("services/",include("services.urls")),
-    path("api/", include("chats.urls")),
     path("admin/", admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path("api/inquiries/", include("inquiries.urls")),
-
 ]
