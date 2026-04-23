f18aea8b1ff53024b4ab796d0567a92b9ba85f1e
Bailey Yi <byi@nec-labs.com>
2026-04-23 17:53:57 -0400

    fix: fixed the websocket caht routin and message persistence

--- backend/chats/routing.py
+++ backend/chats/routing.py
@@ -4,5 +4,5 @@ from .consumers import ChatConsumer
 
 websocket_urlpatterns=[
     #call the as_asgi() - get an ASGI aplication that will instantiate an instance of our consumer for each user-connection
-    re_path(r"^ws/inquries/(?P<inquiry_id>\d+)/$", ChatConsumer.as_asgi()),
+    re_path(r"^ws/inquiries/(?P<inquiry_id>\d+)/$", ChatConsumer.as_asgi()),
 ]
