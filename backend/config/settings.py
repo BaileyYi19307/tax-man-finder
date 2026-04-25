557dad0ba663a0e1a2d41081448b5a91e308389d
Bailey Yi <byi@nec-labs.com>
2026-04-25 10:33:35 -0400

    Add inquiry read-state tracking

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -80,6 +80,14 @@ REST_FRAMEWORK = {
     )
 }
 
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+}
+
+
 AUTH_USER_MODEL="users.User"
 
 CORS_ALLOWED_ORIGINS = [
--- backend/inquiries/urls.py
+++ backend/inquiries/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import ListCreateInquiriesView
 from .views import ReadSpecificInquiryView
-from .views import SendMessageView
+from .views import SendMessageView, MarkReadView
 
 
 urlpatterns = [
     path("", ListCreateInquiriesView.as_view(), name="list-create-inquiries"),
     path("<int:inquiry_id>/", ReadSpecificInquiryView.as_view(),name="read-specific-inquiry"),
-    path("<int:inquiry_id>/messages/", SendMessageView.as_view(), name="send-message")
+    path("<int:inquiry_id>/messages/", SendMessageView.as_view(), name="send-message"),
+    path("<int:inquiry_id>/mark-read/",MarkReadView.as_view(),name="mark-specific-inquiry-read" )
 ]
